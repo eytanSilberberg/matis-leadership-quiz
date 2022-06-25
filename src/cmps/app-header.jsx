ab84156552e7cd5a8f712fca7038009effb6da7b
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-06-25 14:59:40 +0300

    added layout

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const AppHeader = () => {
+
+    const [isMenuOpen, setMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setMenu(!isMenuOpen)
+    }
+
+    let checkSize = () => {
+        console.log(window.innerWidth);
+        if (window.innerWidth > 600) {
+            setMenu(false)
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('resize', checkSize)
+    }, [])
+
     return <header className='full'>
-        <section className='header-content-wrapper flex space-between'>
+        {isMenuOpen && <div onClick={toggleMenu} className='clear-screen'></div>}
+        <section className='header-content-wrapper flex'>
             <div className='logo'>Logo</div>
-            <nav>
+            <nav className={isMenuOpen ? 'open' : ''}>
                 <NavLink to={'/'}>Home</NavLink>
                 <NavLink to={'/about'}>About</NavLink>
             </nav>
+            <button onClick={toggleMenu}>☰</button>
+
         </section>
     </header>
 }
