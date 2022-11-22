52cd78fd291e09cc021060f029564a1ce312c6c8
Eytan Silberberg <eytan@Eytans-MacBook-Pro.local>
2022-11-22 08:58:36 +0200

    fixed bug- table mui pagination didnt work

--- src/cmps/mui.table.jsx
+++ src/cmps/mui.table.jsx
@@ -17,7 +17,7 @@ export const MuiTable = ({ forms, windowSize, onViewUsersForm }) => {
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
 
-    const handleChangePage = (newPage) => {
+    const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
 
